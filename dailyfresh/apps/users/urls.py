663a6aa393d751f7fa018fea34860c8fe935d498
zy <neilasbefore@163.com>
2018-03-06 22:06:07 +0800

    注册逻辑2

---
+++ dailyfresh/apps/users/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from users import views
+
+urlpatterns = [
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^active/(?P<token>.+)$', views.ActiveView.as_view(), name='active'),
+]
